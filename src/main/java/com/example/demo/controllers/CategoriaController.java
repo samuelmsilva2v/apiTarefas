a25da7820bfc1d76ba7634bbcff7874c6e4f9622
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-08-13 15:09:21 -0300

    Swagger dependency and tarefa and categoria controllers added

--- src/main/java/com/example/demo/controllers/CategoriaController.java
+++ src/main/java/com/example/demo/controllers/CategoriaController.java
@@ -1,33 +1,82 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dtos.CategoriaDto;
+import com.example.demo.entities.Categoria;
+import com.example.demo.repositories.CategoriaRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/categorias")
 public class CategoriaController {
 
 	@PostMapping
-	public void post() throws Exception {
-		
+	public String post(@RequestBody @Valid CategoriaDto dto) throws Exception {
+
+		Categoria categoria = new Categoria();
+		categoria.setNome(dto.getNome());
+
+		CategoriaRepository categoriaRepository = new CategoriaRepository();
+		categoriaRepository.insert(categoria);
+
+		return "Categoria cadastrada com sucesso!";
 	}
-	
-	@PutMapping
-	public void put() throws Exception {
-		
+
+	@PutMapping("{id}")
+	public String put(@PathVariable Integer id, @RequestBody @Valid CategoriaDto dto) throws Exception {
+
+		CategoriaRepository categoriaRepository = new CategoriaRepository();
+		Categoria categoria = categoriaRepository.findById(id);
+
+		if (categoria == null)
+			return "Categoria não encontrada, verifique o ID informado.";
+
+		categoria.setNome(dto.getNome());
+		categoriaRepository.update(categoria);
+
+		return "Categoria atualizada com sucesso.";
 	}
-	
-	@DeleteMapping
-	public void delete() throws Exception {
-		
+
+	@DeleteMapping("{id}")
+	public String delete(@PathVariable Integer id) throws Exception {
+
+		CategoriaRepository categoriaRepository = new CategoriaRepository();
+		Categoria categoria = categoriaRepository.findById(id);
+
+		if (categoria == null)
+			return "Categoria não encontrada, verifique o ID informado.";
+
+		categoriaRepository.delete(id);
+
+		return "Categoria excluída com sucesso!";
 	}
-	
+
 	@GetMapping
-	public void getAll() throws Exception {
+	public List<Categoria> getAll() throws Exception {
+
+		CategoriaRepository categoriaRepository = new CategoriaRepository();
+		List<Categoria> lista = categoriaRepository.findAll();
+
+		return lista;
+	}
+
+	@GetMapping("{id}")
+	public Categoria getById(@PathVariable Integer id) throws Exception {
 		
+		CategoriaRepository categoriaRepository = new CategoriaRepository();
+		Categoria categoria = categoriaRepository.findById(id);
+		
+		return categoria;
 	}
 }
--- src/main/java/com/example/demo/controllers/TarefaController.java
+++ src/main/java/com/example/demo/controllers/TarefaController.java
@@ -1,33 +1,96 @@
 package com.example.demo.controllers;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dtos.TarefaDto;
+import com.example.demo.entities.Categoria;
+import com.example.demo.entities.Tarefa;
+import com.example.demo.repositories.TarefaRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/tarefas")
 public class TarefaController {
 
 	@PostMapping
-	public void post() throws Exception {
+	public String post(@RequestBody @Valid TarefaDto dto) throws Exception {
+
+		Tarefa tarefa = new Tarefa();
+		tarefa.setCategoria(new Categoria());
+
+		tarefa.setNome(dto.getNome());
+		tarefa.setData(dto.getData());
+		tarefa.setDescricao(dto.getDescricao());
+		tarefa.getCategoria().setId(dto.getCategoriaId());
+
+		TarefaRepository tarefaRepository = new TarefaRepository();
+		tarefaRepository.insert(tarefa);
+
+		return "Tarefa cadastrada com sucesso!";
+	}
+
+	@PutMapping("{id}")
+	public String put(@PathVariable Integer id, @RequestBody @Valid TarefaDto dto) throws Exception {
+
+		TarefaRepository tarefaRepository = new TarefaRepository();
+		Tarefa tarefa = tarefaRepository.findById(id);
+
+		if (tarefa == null)
+			return "Tarefa não encontrada, verifique o ID informado.";
+
+		tarefa.setNome(dto.getNome());
+		tarefa.setData(dto.getData());
+		tarefa.setDescricao(dto.getDescricao());
+		tarefa.getCategoria().setId(dto.getCategoriaId());
+		tarefaRepository.update(tarefa);
+
+		return "Tarefa atualizada com sucesso!";
+	}
+
+	@DeleteMapping("{id}")
+	public String delete(@PathVariable Integer id) throws Exception {
+
+		TarefaRepository tarefaRepository = new TarefaRepository();
+		Tarefa tarefa = tarefaRepository.findById(id);
+
+		if (tarefa == null)
+			return "Tarefa não encontrada, verifique o ID informado.";
+
+		tarefaRepository.delete(id);
+
+		return "Tarefa excluída com sucesso!";
+	}
+
+	@GetMapping("{dataMin}/{dataMax}")
+	public List<Tarefa> getAll(
+			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date dataMin, 
+			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date dataMax)
+			throws Exception {
+
+		TarefaRepository tarefaRepository = new TarefaRepository();
+		List<Tarefa> lista = tarefaRepository.findAll(dataMin, dataMax);
 		
+		return lista;
 	}
 	
-	@PutMapping
-	public void put() throws Exception {
+	@GetMapping("{id}")
+	public Tarefa getById(@PathVariable Integer id) throws Exception {
 		
-	}
-	
-	@DeleteMapping
-	public void delete() throws Exception {
+		TarefaRepository tarefaRepository = new TarefaRepository();
+		Tarefa tarefa = tarefaRepository.findById(id);
 		
-	}
-	
-	@GetMapping
-	public void getAll() throws Exception {
-		
+		return tarefa;
 	}
 }
